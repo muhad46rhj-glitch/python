e80852ef5f25757dc19291eba5a67354aaf36c4e
muhad46rhj-glitch <muhad46rhj@gmail.com>
2026-06-03 21:18:10 +0100

    Add files via upload

---
+++ homework of Veriable.py
@@ -0,0 +1,11 @@
+x = "essa - 4 september 2015"
+y = "ahmad - 24 december 2012"
+z = "hadi - 16 feburary 2015"
+m = "musa - 12 - january 2011"
+a = " mujhtaba - 25 -november 2016"
+
+print("essa birthday:" , x)
+print("ahmad birthday:" , y)
+print("hadi  birthday:" , z)
+print("musa birthday:" , m)
+print(" mujhtaba birthday:" , a)
