b2f6528ead2ca83a32543c1b8ece552d8382a8e9
muhad46rhj-glich <muhad46rhj@gmail.com>
2026-06-03 18:17:57 +0100

    print statement

---
+++ python/print statement.py
@@ -0,0 +1,14 @@
+#print a sentance 
+print("welcome to world of programig!")
+      
+#print a Number
+print(4)
+
+#Next line after print
+print("hello python \n")
+
+#pass multiple argumets for printing
+print("hello",5)
+
+#End argument of print statement
+print("welcome to",end="*")
---
+++ python/variable.py
@@ -0,0 +1,13 @@
+#Creating diffrent variables
+x = 5
+y = "John"
+print(x)
+print(y)
+
+codingal = "codingal"
+print (codingal)
+
+#USER INPUT
+name = input ("enter your name:")
+
+print("\nhello",name,"\nwelcome to codingal")
