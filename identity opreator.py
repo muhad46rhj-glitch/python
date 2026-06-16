6368b8c68713471a4ec1107cc4cac4cd8c462152
muhad46rhj-glitch <muhad46rhj@gmail.com>
2026-06-16 17:35:26 +0100

    Add files via upload

---
+++ identity opreator.py
@@ -0,0 +1,35 @@
+x = 5
+
+if (type(x) is int):
+
+    print("true")
+
+else:
+
+    print("false")
+
+
+x = 5.5
+
+if (type(x)is not float):
+
+    print("true")
+
+else:
+
+    print("false")
+
+
+x = 20
+
+y = 20
+
+if (x is y):
+
+    print("x & y SAME IDENTIFY")
+
+
+    y = 30
+
+if (x is not y):
+    print("x & y have DIFFRENT IDENTIFY")
