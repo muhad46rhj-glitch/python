0dc369bee6fc998cbae9f8b8bffaf1c1f670c168
muhad46rhj-glitch <muhad46rhj@gmail.com>
2026-06-17 17:27:39 +0100

    Add files via upload

---
+++ opreator president.py
@@ -0,0 +1,17 @@
+v = 4
+w = 5
+x = 8
+y = 2
+z = 0
+z = (v+w) * x / y;
+print("Value of(v+w) * x/ y is", z)
+
+
+name = "Alex rider"
+Age = 0
+
+if name == "Alex" or name == "jack" and age >= 12 :
+    print("Hello welcome")
+
+else:
+    print("good bye!!")
