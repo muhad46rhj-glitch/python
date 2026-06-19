6f5ad7c1ddf05f58b921e6567e79ab81e77977bc
muhad46rhj-glitch <muhad46rhj@gmail.com>
2026-06-19 18:31:38 +0100

    Add files via upload

---
+++ daily routie printer.py
@@ -0,0 +1,12 @@
+# Daily Routine Printer
+
+print("=== My Daily Routine ===")
+print("6:30 AM - Wake up")
+print("7:00 AM - Breakfast")
+print("8:00 AM - School")
+print("3:30 PM - Homework")
+print("5:00 PM - Play / Exercise")
+print("6:30 PM - Dinner")
+print("7:30 PM - Study")
+print("9:00 PM - Relax")
+print("10:00 PM - Sleep")
