b2ae5c30d051560d4ed78d31c90a008bc3bd664c
muhad46rhj-glitch <muhad46rhj@gmail.com>
2026-07-08 17:30:52 +0100

    Add files via upload

---
+++ break.py
@@ -0,0 +1,11 @@
+#take user input
+a = input ("enter a word: ")
+#program to check break keyword
+for i in a: #iterate for loop
+    if (i == 'A'): #codition 1
+    #display result
+      print ("A is found")
+      break #break statement
+    else:
+       #display result
+           print ("A not found")
