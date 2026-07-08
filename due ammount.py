04d70abcd50088458c3f0c93e8a90b6a12e92aeb
muhad46rhj-glitch <muhad46rhj@gmail.com>
2026-07-08 18:02:12 +0100

    Add files via upload

---
+++ due ammount.py
@@ -0,0 +1,9 @@
+# Input
+bill_amount = float(input("Enter bill amount: "))
+paid_amount = float(input("Enter paid amount: "))
+
+# Calculate due amount
+due_amount = bill_amount - paid_amount
+
+# Output
+print("Due Amount:", due_amount)
