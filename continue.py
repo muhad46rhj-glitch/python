f93c68bb0eaa6af35e2c6b668bc18a035e566672
muhad46rhj-glitch <muhad46rhj@gmail.com>
2026-07-08 17:21:45 +0100

    Add files via upload

---
+++ continue.py
@@ -0,0 +1,9 @@
+var = 10           #initialise
+while var > 0:     #interate loop
+       var = var - 1
+       if var ==5:      #coditional 1
+           continue     #continue statement
+           #display result
+       print ('/ncurrent variable value :', var)
+print ("/nGood bye!")
+    
