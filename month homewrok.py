e8f94f0669a468d052373a0b757a88d45ffa7727
muhad46rhj-glitch <muhad46rhj@gmail.com>
2026-07-15 18:05:20 +0100

    Add files via upload

---
+++ month homewrok.py
@@ -0,0 +1,11 @@
+# Program to display all the month's names
+
+months = [
+    "January", "February", "March", "April",
+    "May", "June", "July", "August",
+    "September", "October", "November", "December"
+]
+
+print("Months of the Year:")
+for month in months:
+    print(month)
