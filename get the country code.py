1e7b3ab94b59e2e2a7dd6e0143837d2062a46df9
muhad46rhj-glitch <muhad46rhj@gmail.com>
2026-07-22 18:08:07 +0100

    Add files via upload

---
+++ get the country code.py
@@ -0,0 +1,8 @@
+country_code = {'India' : '0091',
+                'Australia' : '0023'
+                'Nepal'  '00988'}
+print("Country code for india -")
+print(country_code.get('India', 'Not Found'))
+
+print("Country code fr japan -")
+print(country_code.get('Japan', 'Not found'))
